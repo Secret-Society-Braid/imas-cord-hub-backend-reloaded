025978acdd58c50968150039f112fe3f73daaafb
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-09-26 21:28:58 +0900

    add/fix tests

--- src/test/java/org/braid/society/secret/imascordhubbackend/servers/ServerRepositoryTest.java
+++ src/test/java/org/braid/society/secret/imascordhubbackend/servers/ServerRepositoryTest.java
@@ -2,9 +2,16 @@ package org.braid.society.secret.imascordhubbackend.servers;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
 import org.braid.society.secret.imascordhubbackend.api.IP;
+import org.braid.society.secret.imascordhubbackend.api.database.DatabaseOperation;
 import org.braid.society.secret.imascordhubbackend.api.entity.ServerEntity;
+import org.braid.society.secret.imascordhubbackend.internal.database.ServerDatabaseOperation;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -12,25 +19,36 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class ServerRepositoryTest {
 
+  BufferedReader br;
+  DatabaseOperation<ServerEntity> op;
+  ServerRepository r;
+
+  @BeforeEach
+  void setUp() {
+    br = new BufferedReader(
+      new InputStreamReader(
+        Objects.requireNonNull(getClass().getResourceAsStream("/server.model.csv")),
+        StandardCharsets.UTF_8));
+    op = new ServerDatabaseOperation(br);
+    r = new ServerRepository(op);
+  }
+
   @Test
   void getAllShouldReturnAllServers() {
     // setup
-    final ServerRepository r = new ServerRepository();
     final List<ServerEntity> list = r.getAllServer();
 
     // assertion
     assertThat(list).isNotNull();
     assertThat(list).isNotEmpty();
-    assertThat(list).hasSize(5);
+    assertThat(list).hasSize(4);
 
     System.out.println(list);
   }
 
-  //TODO: add rest of tests
   @Test
   void searchServersShouldReturnCorrectly() {
     // setup
-    final ServerRepository r = new ServerRepository();
     final List<ServerEntity> list = r.searchServers("name", "デレマス");
 
     // assertion
@@ -44,7 +62,6 @@ class ServerRepositoryTest {
   @Test
   void findShouldOnlyReturnOneEntity() {
     // setup
-    final ServerRepository r = new ServerRepository();
     final ServerEntity obj = r.getServerById("9bffce54-d95e-4f51-b8d1-8b68aaea605a");
 
     // assertion
@@ -56,14 +73,21 @@ class ServerRepositoryTest {
   }
 
   @Test
-  void randomShouldReturnSpecifiedNumberOfEntity() {
+  void lessRandomShouldReturnSpecifiedNumberOfEntity() {
     // setup
-    final ServerRepository r = new ServerRepository();
     final List<ServerEntity> lessList = r.getRandomServers(2);
-    final List<ServerEntity> moreList = r.getRandomServers(10);
 
     // assertion
     assertThat(lessList).hasSize(2);
-    assertThat(moreList).hasSize(10);
+  }
+
+  @Test
+  void moreRandomShouldReturnSpecifiedNumberOfEntity() {
+    // setup
+    final ServerRepository r = new ServerRepository(op);
+    final List<ServerEntity> lessList = r.getRandomServers(10);
+
+    // assertion
+    assertThat(lessList).hasSize(10);
   }
 }
