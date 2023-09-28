a91664ba95e0a88918feefe6f6d1b4dc434344ed
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-09-28 14:26:05 +0900

    fix database operation logic by test results

--- src/main/java/org/braid/society/secret/imascordhubbackend/servers/ServerRepository.java
+++ src/main/java/org/braid/society/secret/imascordhubbackend/servers/ServerRepository.java
@@ -6,6 +6,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.braid.society.secret.imascordhubbackend.api.database.DatabaseOperation;
@@ -18,32 +19,40 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class ServerRepository {
 
-  private final DatabaseOperation<ServerEntity> op;
+  private final String dbPath;
 
   public ServerRepository() {
+    this("/static/server.model.csv");
+  }
+
+  private DatabaseOperation<ServerEntity> createOperator() {
     BufferedReader br = new BufferedReader(new InputStreamReader(
-      Objects.requireNonNull(this.getClass().getResourceAsStream("/static/server.model.csv")),
+      Objects.requireNonNull(ServerRepository.class.getResourceAsStream(this.dbPath)),
       StandardCharsets.UTF_8));
-    this.op = new ServerDatabaseOperation(br);
+    return new ServerDatabaseOperation(br);
   }
 
   public List<ServerEntity> getAllServer() {
-    return this.op.getAll();
+    final DatabaseOperation<ServerEntity> op = createOperator();
+    return op.getAll();
   }
 
   public ServerEntity getServerById(String id) {
-    return this.op.get(id);
+    final DatabaseOperation<ServerEntity> op = createOperator();
+    return op.get(id);
   }
 
   public List<ServerEntity> searchServers(String fieldName, String term) {
-    return this.op.filter(fieldName, term);
+    final DatabaseOperation<ServerEntity> op = createOperator();
+    return op.filter(fieldName, term);
   }
 
   public List<ServerEntity> getRandomServers(int count) {
     if (count <= 0) {
       throw new IllegalArgumentException("count must be positive");
     }
-    List<ServerEntity> servers = this.op.getAll();
+    final DatabaseOperation<ServerEntity> op = createOperator();
+    List<ServerEntity> servers = op.getAll();
     if (count > servers.size()) {
       int size = servers.size();
       for (int i = 0; i < count - size; i++) {
