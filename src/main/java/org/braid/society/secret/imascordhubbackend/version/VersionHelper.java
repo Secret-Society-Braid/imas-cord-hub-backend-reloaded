824757d224bb44df0ce0c067f33551115c72a04a
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-29 20:03:16 +0900

    add helper class for version endpoint

--- src/main/java/org/braid/society/secret/imascordhubbackend/version/VersionHelper.java
+++ src/main/java/org/braid/society/secret/imascordhubbackend/version/VersionHelper.java
@@ -1,12 +1,17 @@
 package org.braid.society.secret.imascordhubbackend.version;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@Getter
 public class VersionHelper {
 
-  public String getFullVersion() {
-    return "";
-  }
+  private final int major = 0;
+  private final int minor = 0;
+  private final int patch = 0;
 
+  public String getFullVersion() {
+    return String.format("%d.%d.%d", major, minor, patch);
+  }
 }
